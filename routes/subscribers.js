665adc06dffd16f1a65d0d9990142eb2cc40df8d
Anmol Bhalla <anmolbhalla@uvic.ca>
2025-01-31 18:11:36 -0800

    Fixed typo

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -27,13 +27,13 @@ router.get('/:id', async (req, res) => {
 
 // Create a subscriber
 router.post('/', async (req, res) => {
-    const subsriber = new Subscriber({
+    const subscriber = new Subscriber({
         name: req.body.name,
         subscribedToChannel: req.body.subscribedToChannel
     });
 
     try {
-        const newSubscriber = await subsriber.save();
+        const newSubscriber = await subscriber.save();
         res.status(201).json(newSubscriber);
     } catch (err) {
         res.status(400).json({ message: err.message });
